a61e03cf5c2d4630b6ecfee45b3decde86332e7d
Heather Miller <heather.miller114@gmail.com>
2018-09-27 15:00:03 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,3 +4,13 @@ class Appointment
   @@all = []
   
   def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
